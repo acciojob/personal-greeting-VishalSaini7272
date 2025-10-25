6ce9c4c2e2fdcb784c291958d76712898a22c686
VishalSaini7272 <sainivishal156@gmail.com>
2025-10-25 21:54:42 +0530

    Add name input and greeting functionality

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,17 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName ] = useState('')
   return (
     <div>
         {/* Do not remove the main div */}
+     <p>Enter your name:</p>
+     <input
+       type="text"
+       value={name}
+       onChange={(e) => setName(e.target.value)}
+     />
+     {name ? <p>Hello {name}!</p> : null}
     </div>
   )
 }
